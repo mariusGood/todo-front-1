e715f38aa963c036525a48036ccfb71e92e49829
margus0 <105805741+margus0@users.noreply.github.com>
2022-06-15 18:45:31 +0300

    Update Input.jsx

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,25 +4,24 @@ import * as S from './Input.styles';
 import Button from '../Button/Button';
 import { postData } from '../../utils/services';
 
-const Input = ({ children, text }) => {
+const Input = ({ children, reload }) => {
   const [data, setData] = useState([]);
-
-  const formHandle = async () => {
+  async function formHandle(e) {
+    e.preventDefault();
     const inputData = {
       title: data,
     };
     const res = await postData(inputData);
-    return res;
-  };
+
+    if (res.msg === 'Success') {
+      setData('');
+      reload();
+    }
+  }
 
   return (
     <S.InputWrapper onSubmit={formHandle}>
-      <S.Input
-        placeholder={text}
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-         onClick={(e) => setData(e.target.value)}
-      >
+      <S.Input value={data} onChange={(e) => setData(e.target.value)}>
         {children}
       </S.Input>
       <Button type='submit' color='#9c4700' bg='#f9bf90'>
@@ -34,7 +33,7 @@ const Input = ({ children, text }) => {
 
 Input.propTypes = {
   children: PropTypes.any,
-  text: PropTypes.string,
+  reload: PropTypes.func,
 };
 
 export default Input;
